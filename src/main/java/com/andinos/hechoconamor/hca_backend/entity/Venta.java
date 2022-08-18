2cf1714939b1fe82d774a7318a27a76a4c922452
Franco <franco.racing.cardozo@gmail.com>
2022-08-18 01:26:35 -0300

    entityVenta

--- src/main/java/com/andinos/hechoconamor/hca_backend/entity/Venta.java
+++ src/main/java/com/andinos/hechoconamor/hca_backend/entity/Venta.java
@@ -1,5 +1,68 @@
 package com.andinos.hechoconamor.hca_backend.entity;
 
-public class Venta {
-    
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Venta implements Serializable {
+
+    private static final int serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="idUsuario", nullable=false)
+    private Usuario usuario;
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @Column(name = "create_time")
+    private Date createTime;
+
+    private BigDecimal monto;
+
+    public Venta(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public BigDecimal getMonto() {
+        return monto;
+    }
+
+    public void setMonto(BigDecimal monto) {
+        this.monto = monto;
+    }
+    @PrePersist
+    public void prePersist() {
+        createTime = new Date();
+    }
 }
