e7bf298e4ef94f22c3e7c923e2460abbd9fa2bff
diebo <diegoestrada35@gmail.com>
2022-08-18 01:29:38 -0300

    Producto entity

--- src/main/java/com/andinos/hechoconamor/hca_backend/entity/Producto.java
+++ src/main/java/com/andinos/hechoconamor/hca_backend/entity/Producto.java
@@ -1,7 +1,40 @@
 package com.andinos.hechoconamor.hca_backend.entity;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class Producto {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idProducto;
+
+    private String nombre;
+    private String shortDescription;
+    private String fullDescription;
+    private String imagen;
+    
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @Column(name = "create_time")
+    private Date fechaYhora;
+
+    private Integer PorcentajeDescuento;
+    private Float largo;
+    private Float ancho;
+    private Float peso;
+
+    @OneToOne
+    private Venta venta;
+
+
 }
