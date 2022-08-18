a5287dc5445af603559a82c587c3b1ea531863b9
diebo <diegoestrada35@gmail.com>
2022-08-17 23:28:35 -0300

    first commit

---
+++ src/main/java/com/andinos/hechoconamor/hca_backend/HcaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.andinos.hechoconamor.hca_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andinos/hechoconamor/hca_backend/HcaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andinos.hechoconamor.hca_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
