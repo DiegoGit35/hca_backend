9ee3f451d43e8b694d975cb316085f9a20aa3f88
Franco <franco.racing.cardozo@gmail.com>
2022-08-19 01:21:15 -0300

    actualiza

--- src/main/java/com/andinos/hechoconamor/hca_backend/controller/ProductoController.java
+++ src/main/java/com/andinos/hechoconamor/hca_backend/controller/ProductoController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/producto")
 public class ProductoController {
 
-
     private IProductoService productoService;
 
     @GetMapping
